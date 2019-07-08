7d9c35db56c6cd534d1d66b4275f9b9f4950a313
Carlos Sanchez <caaugustoss@gmail.com>
2019-07-08 17:37:21 +0200

    UP-679 #comment Adding Device Generator v0.2

--- src/main/scala/com/ubirch/DeviceGenerator.scala
+++ src/main/scala/com/ubirch/DeviceGenerator.scala
@@ -10,6 +10,7 @@ import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
+import org.json4s.DefaultFormats
 import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods._
 
@@ -17,30 +18,15 @@ import scala.annotation.tailrec
 
 object DeviceGenerator extends ConfigBase with DeviceGenerationFileConfigs with LazyLogging {
 
-  val auth = Base64.getEncoder.encodeToString(("devicebootstrap:" + "Fhdt1bb1f").getBytes)
+  implicit val formats = DefaultFormats
+
+  def encode(data: String) = Base64.getEncoder.encodeToString(data.getBytes)
+
+  val auth = encode("devicebootstrap:" + "Fhdt1bb1f")
 
   val client: HttpClient = HttpClients.createMinimal()
 
-  @tailrec
-  def check(request: HttpPost): Unit = {
-    val response = client.execute(request)
-    val code = response.getStatusLine.getStatusCode
-    readEntity(response)
-    if (code == 404) {
-      Thread.sleep(5000)
-      check(request)
-    } else if (code == 201) {}
-    else {
-      logger.error("Unexpected response code: " + code)
-    }
-  }
-
-  def readEntity(response: HttpResponse) = {
-    val res = EntityUtils.toString(response.getEntity)
-    logger.info(res)
-  }
-
-  def request(data: String) = {
+  def deviceCredentialsRequest(data: String) = {
     val req = new HttpPost("https://ubirch.cumulocity.com/devicecontrol/deviceCredentials")
     req.addHeader("Content-Type", "application/vnd.com.nsn.cumulocity.deviceCredentials+json")
     req.addHeader("Accept", "application/vnd.com.nsn.cumulocity.deviceCredentials+json")
@@ -49,17 +35,99 @@ object DeviceGenerator extends ConfigBase with DeviceGenerationFileConfigs with 
     req
   }
 
+  def deviceInventoryRequest(data: String, username: String, password: String) = {
+    val req = new HttpPost("https://ubirch.cumulocity.com/inventory/managedObjects")
+    req.addHeader("Content-Type", "application/vnd.com.nsn.cumulocity.managedObject+json")
+    req.addHeader("Accept", "application/vnd.com.nsn.cumulocity.managedObject+json")
+    req.addHeader("Authorization", "Basic " + encode(s"$username:$password"))
+    req.setEntity(new StringEntity(data))
+    req
+  }
+
+  def deviceExternalIdRequest(id: String, data: String, username: String, password: String) = {
+    val req = new HttpPost(s"https://ubirch.cumulocity.com/identity/globalIds/$id/externalIds")
+    req.addHeader("Content-Type", "application/vnd.com.nsn.cumulocity.externalId+json")
+    req.addHeader("Accept", "application/vnd.com.nsn.cumulocity.externalId+json")
+    req.addHeader("Authorization", "Basic " + encode(s"$username:$password"))
+    req.setEntity(new StringEntity(data))
+    req
+  }
+
+  def getDeviceCredentials(response: String) = {
+    ((parse(response) \ "username").extract[String], (parse(response) \ "password").extract[String])
+  }
+
+  def getExternalId(response: String) = {
+    (parse(response) \ "id").extract[String]
+  }
+
+  def readEntity(response: HttpResponse) = {
+    EntityUtils.toString(response.getEntity)
+  }
+
+  def externalIdData(uuid: UUID) = {
+    s"""{"type":"c8y_Serial","externalId":"$uuid"}"""
+  }
+
+  def deviceCredentialsData(uuid: UUID) = {
+    compact(render("id" -> uuid.toString))
+  }
+
+  def deviceInventoryData(id: String) =
+    s"""
+       |{
+       |  "name": "$id",
+       |  "type": "DEVICE",
+       |  "c8y_IsDevice":{},
+       |  "c8y_Hardware":{
+       |    "revision": "v1.0",
+       |    "serialNumber": "$id"
+       |  }
+       |}
+    """.stripMargin
+
+  @tailrec
+  def register(uuid: UUID): Unit = {
+    val response = client.execute(deviceCredentialsRequest(deviceCredentialsData(uuid)))
+    val code = response.getStatusLine.getStatusCode
+    val entityAsString = readEntity(response)
+    logger.info(entityAsString)
+    if (code == 404) {
+      Thread.sleep(5000)
+      register(uuid)
+    } else if (code == 201) {
+      val (u, p) = getDeviceCredentials(entityAsString)
+      val data = deviceInventoryData(uuid.toString)
+      val response = client.execute(deviceInventoryRequest(data, u, p))
+      val entityAsString2 = readEntity(response)
+      val code = response.getStatusLine.getStatusCode
+      logger.info(entityAsString2)
+      if (code < 300) {
+        val id = getExternalId(entityAsString2)
+        val data = externalIdData(uuid)
+        val response = client.execute(deviceExternalIdRequest(id, data, u, p))
+        val entityAsString3 = readEntity(response)
+        logger.info(entityAsString3)
+        if (code < 300) {
+          logger.info("Device registered")
+        }
+      } else {
+        logger.error("Unexpected response code (1): " + code)
+      }
+    } else {
+      logger.error("Unexpected response code (2): " + code)
+    }
+  }
+
   def main(args: Array[String]): Unit = {
 
-    val uuids = (0 to numberOfDevices).map { _ => UUID.randomUUID() }
-
-    val data = uuids.map(x => compact(render("id" -> x.toString)))
+    val uuids = (1 to numberOfDevices).map { _ => UUID.randomUUID() }
 
     logger.info(s"Starting process for ${uuids.size} devices")
-    data.foreach { x =>
-      logger.info(x)
-      Thread.sleep(3000)
-      check(request(x))
+    uuids.foreach { x =>
+      logger.info(x.toString)
+      Thread.sleep(10000)
+      register(x)
     }
 
   }
