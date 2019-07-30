907e1e0943d675731cec877767143ae9413307bf
Matthias L. Jugel <leo@thinkberg.com>
2019-07-30 17:01:51 +0200

    change to make it more configurable

--- src/test/scala/com/ubirch/simulations/SendUPPAproxRateSecondsSimulation.scala
+++ src/test/scala/com/ubirch/simulations/SendUPPAproxRateSecondsSimulation.scala
@@ -8,13 +8,17 @@ import scala.language.postfixOps
 
 class SendUPPAproxRateSecondsSimulation
   extends Simulation
-  with SendUPP
-  with Protocols
-  with ConfigBase {
+    with SendUPP
+    with Protocols
+    with ConfigBase {
 
   val numberOfUsers: Int = conf.getInt("sendUPPAproxRateSecondsSimulation.numberOfUsers")
   val duringValue: Int = conf.getInt("sendUPPAproxRateSecondsSimulation.duringValue")
   val devices: List[String] = conf.getString("simulationDevices").split(",").toList.filter(_.nonEmpty)
+
+  val rps: Int = conf.getInt("sendUPPAproxRateSecondsSimulation.rps")
+  val rampUp: Int = conf.getInt("sendUPPAproxRateSecondsSimulation.rampup")
+  val length: Int = conf.getInt("sendUPPAproxRateSecondsSimulation.length")
 
   setUp(
     sendScenario(devices)
@@ -23,6 +27,11 @@ class SendUPPAproxRateSecondsSimulation
           //if set to 14, it will send 14 messages per second
           .during(duringValue seconds)
       )
+      .throttle(
+        reachRps(rps) in (rampUp seconds),
+        holdFor(length seconds),
+      )
+
   ).protocols(niomonProtocol)
 
 }
