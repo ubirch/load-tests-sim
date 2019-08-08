f0f7c6df4a21fc060dc0d02c6a20f13530146ac2
Matthias L. Jugel <leo@thinkberg.com>
2019-08-08 08:20:41 +0200

    generate more data to accomodate tests, share connections, better test case handling

--- src/test/scala/com/ubirch/simulations/SendUPPAproxRateSecondsSimulation.scala
+++ src/test/scala/com/ubirch/simulations/SendUPPAproxRateSecondsSimulation.scala
@@ -16,14 +16,14 @@ class SendUPPAproxRateSecondsSimulation
   val duringValue: Int = conf.getInt("sendUPPAproxRateSecondsSimulation.duringValue")
   val devices: List[String] = conf.getString("simulationDevices").split(",").toList.filter(_.nonEmpty)
 
-  setUp(
-    sendScenario(devices)
-      .inject(
-        constantUsersPerSec(numberOfUsers) //The number of users here can be thought of as the number of req/s.
-          //if set to 14, it will send 14 messages per second
-          .during(duringValue seconds)
-      )
-  ).protocols(niomonProtocol)
+  // TODO make configurable
+  setUp(sendScenario(devices).inject(constantUsersPerSec(1000).during(15 minutes)))
+    .throttle(
+      reachRps(1000) in (1 minute),
+      holdFor(2 minute),
+      reachRps(2000) in (1 minute),
+      holdFor(2 minutes)
+    ).protocols(niomonProtocol)
 
 }
 
