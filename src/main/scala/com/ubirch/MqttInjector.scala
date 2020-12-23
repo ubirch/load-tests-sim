12721ba2518f766b5ac7f449e96b1b9da4bf2e8c
Carlos Sanchez <caaugustoss@gmail.com>
2020-12-23 11:40:28 +0100

    Making only one subscriber read responses.

--- src/main/scala/com/ubirch/MqttInjector.scala
+++ src/main/scala/com/ubirch/MqttInjector.scala
@@ -2,10 +2,10 @@ package com.ubirch
 
 import com.google.protobuf.ByteString
 import com.typesafe.scalalogging.LazyLogging
-import com.ubirch.models.{DeviceGeneration, FlowInPayload, FlowOutPayload}
-import com.ubirch.util.{ConfigBase, Continuous, MqttConf}
+import com.ubirch.models.{ DeviceGeneration, FlowInPayload, FlowOutPayload }
+import com.ubirch.util.{ ConfigBase, Continuous, MqttConf }
 import monix.eval.Task
-import org.joda.time.{DateTime, Duration}
+import org.joda.time.{ DateTime, Duration }
 import java.nio.file.Paths
 import java.util.UUID
 import java.util.concurrent.CountDownLatch
@@ -20,6 +20,12 @@ object MqttInjector extends LazyLogging with ConfigBase {
   def inTopic(deviceId: UUID): String = Paths.get(MqttConf.IN_QUEUE_PREFIX, deviceId.toString).toString
   def outTopic(deviceId: UUID): String = Paths.get(MqttConf.OUT_QUEUE_PREFIX, deviceId.toString).toString
   def outAllTopic: String = Paths.get(MqttConf.OUT_QUEUE_PREFIX, "+").toString
+
+  def getClient(cli: String): MqttClients = {
+    val mqtt: MqttClients = new DefaultMqttClients
+    mqtt.buildClient(cli)
+    mqtt
+  }
 
   def subs(max: Int, whenLog: Int, cli: String, mqtt: MqttClients) = {
     val startTime = new DateTime()
@@ -37,9 +43,9 @@ object MqttInjector extends LazyLogging with ConfigBase {
           " status=" + pl.status)
       }
 
-      if(crr == max) {
+      if (crr == max) {
         val endTime = new DateTime()
-        logger.info(s"($cli) DONE - " + new Duration(endTime, startTime).abs().toString)
+        logger.info(s"($cli) DONE - duration=" + new Duration(endTime, startTime).abs().toString + " received=" + received.get())
       }
       Task.delay(m)
     })
@@ -56,15 +62,13 @@ object MqttInjector extends LazyLogging with ConfigBase {
     }
   }
 
-  def go(max: Int, whenLog: Int, cli: String, devices: List[DeviceGeneration]) = {
-    val mqtt: MqttClients = new DefaultMqttClients
-    mqtt.buildClient(cli)
-    subs(max, whenLog, cli, mqtt)
+  def go(max: Int, cli: String, devices: List[DeviceGeneration]) = {
+    val mqtt = getClient(cli)
     pub(max, devices, mqtt)
   }
 
   def splitDevices(maxClients: Int, devices: List[DeviceGeneration]): Vector[List[DeviceGeneration]] = {
-    if(maxClients < devices.size) {
+    if (maxClients < devices.size) {
       val buckets = devices.size / maxClients
       val splits = (0 to maxClients).map { i =>
         devices.slice(buckets * i, buckets * (i + 1))
@@ -86,7 +90,13 @@ object MqttInjector extends LazyLogging with ConfigBase {
     val onlyTheseDevices: List[String] = conf.getString("simulationDevices").split(",").toList.filter(_.nonEmpty)
     val devices: List[DeviceGeneration] = DeviceGenerator.loadDevices(onlyTheseDevices)
     val deviceBuckets: Vector[List[DeviceGeneration]] = splitDevices(maxClients, devices)
-    deviceBuckets.zipWithIndex.par.foreach{ case (devices, i) => go(max, whenLog, cli + i, devices) }
+
+    val buckets = deviceBuckets.size
+    subs(max * buckets, whenLog, "all", getClient("all"))
+
+    logger.info("buckets_size=" + buckets)
+
+    deviceBuckets.zipWithIndex.par.foreach { case (devices, i) => go(max, cli + i, devices) }
 
     new CountDownLatch(1).await()
 
